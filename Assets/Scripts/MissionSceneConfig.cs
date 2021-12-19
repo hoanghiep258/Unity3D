92d96bf53915f9e9500b7fd49bb67e18daacf7c6
Hiep.Nguyen <hiep.nguyen@ezgames.cc>
2021-12-19 17:58:39 +0700

    Add Level design path script Save scene LightProbe and Hiep_ReflectionProbe

---
+++ Assets/Scripts/MissionSceneConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionSceneConfig : MonoBehaviour {
+
+    [SerializeField]
+    public List<PathMove> paths=new List<PathMove>();    
+    // Use this for initialization
+    public void AddNewPathMove()
+    {
+        GameObject go = new GameObject();
+        go.name = "[path_" + paths.Count.ToString() + "]";
+        go.AddComponent<PathMove>();
+        go.transform.SetParent(transform);
+        paths.Add(go.GetComponent<PathMove>());
+    }    
+}
